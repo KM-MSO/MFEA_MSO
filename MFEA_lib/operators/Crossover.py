3ac5ef8884e4cf39f1da6888751758ebadcc430c
haiminh2001 <haiminhnguyen2001@gmail.com>
2022-04-25 21:36:58 +0700

    add pmx

--- MFEA_lib/operators/Crossover.py
+++ MFEA_lib/operators/Crossover.py
@@ -1,6 +1,5 @@
 from typing import Tuple, Type
 import numpy as np
-from sqlalchemy import true
 from ..tasks.task import AbstractTask
 from ..EA import Individual, Population
 from .Search import *
@@ -199,26 +198,92 @@ class newSBX(AbstractCrossover):
 
         return oa, ob
 
+@nb.njit
+def pmx_func(p1, p2, t1, t2,  dim_uss):
+    o = np.empty_like(p1)
+    ob = np.empty_like(p1)
+    
+    mid = np.copy(p2[t1:t2])
+    mid_b = np.copy(p1[t1 : t2])
+    
+    added = np.zeros_like(p1)
+    added_b = np.zeros_like(p2)
+    
+    added[mid] = 1
+    added_b[mid_b] = 1
+    
+    redundant_idx = []
+    redundant_idx_b = []
+    
+    
+    for i in range(t1):
+        if added[p1[i]]:
+            redundant_idx.append(i)
+        else:
+            o[i] = p1[i]
+            added[o[i]] = 1
+            
+        if added_b[p2[i]]:
+            redundant_idx_b.append(i)
+        else:
+            ob[i] = p2[i]
+            added_b[ob[i]] = 1
+            
+    for i in range(t2, dim_uss):
+        
+        if added[p1[i]]:
+            redundant_idx.append(i)
+        else:
+            o[i] = p1[i]
+            added[o[i]] = 1
+            
+        if added_b[p2[i]]:
+            redundant_idx_b.append(i)
+        else:
+            ob[i] = p2[i]
+            added_b[ob[i]] = 1
+    
+    redundant = np.empty(len(redundant_idx))
+    redundant_b = np.empty(len(redundant_idx_b))
+    
+    cnt = 0
+    cnt_b = 0
+    
+    for i in range(t1, t2):
+        if added[p1[i]] == 0:
+            redundant[cnt] = p1[i]
+            cnt+=1
+        if added_b[p2[i]] == 0:
+            redundant_b[cnt_b] = p2[i]
+            cnt_b+=1
+    
+    redundant_idx = np.array(redundant_idx)
+    redundant_idx_b = np.array(redundant_idx_b)
+    
+    o[redundant_idx] = redundant
+    ob[redundant_idx_b] = redundant_b
+    
+    o[t1:t2] = mid
+    ob[t1:t2] = mid_b
+    return o, ob
+    
+
 class PMX_Crossover(AbstractCrossover):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
 
     def __call__(self, pa: Individual, pb: Individual, skf_oa=None, skf_ob=None, *args, **kwargs) -> Tuple[Individual, Individual]:
-        # t1, t2 = np.random.randint(0, self.dim_uss + 1, 2)
+        t1 = np.random.randint(0, self.dim_uss + 1)
+        t2 = np.random.randint(t1, self.dim_uss + 1)
+        genes_o1, genes_o2 = pmx_func(pa.genes, pb.genes, t1, t2, self.dim_uss)
+        o1 = self.IndClass(genes_o1)
+        o2 = self.IndClass(genes_o2)
+        o1.skill_factor = skf_oa
+        o2.skill_factor = skf_ob
+        return o1, o2
 
-        # genes_o1:np.ndarray = np.copy(pa.genes)
-        # genes_o2:np.ndarray = np.copy(pb.genes)
-
-        # mid1 = pb.genes[t1:t2].tolist()
-        # mid2 = pa.genes[t1:t2].tolist()
-
-        # for g in mid1:
-        #     if g not in mid2:
-        #         np.where(pb.genes == g)
-
-        # # NOTE
-        # idx = np.where(genes_o1[:t1] not in genes_o1[t1:t2])[0].tolist() + np.where(genes_o1[t2:] not in genes_o1[t1:t2])[0].tolist()
-        pass
+        
 
 class TPX_Crossover(AbstractCrossover):
     def __call__(self, pa: Individual, pb: Individual, skf_oa=None, skf_ob=None, *args, **kwargs) -> Tuple[Individual, Individual]:
--- test2.py
+++ test2.py
@@ -1,4 +1,14 @@
-import os
+import numpy as np
+from MFEA_lib.operators.Crossover import PMX_Crossover
+from MFEA_lib.tasks.Benchmark.IDPC_EDU import IDPC_EDU_benchmark
+p1 = np.array([9,8,6,4,2,3,7,1,5]) - 1
+p2 = np.array([7,5,2,9,4,8,6,3,1]) - 1
+func = PMX_Crossover()
+tasks, IndClass = IDPC_EDU_benchmark.get_tasks(1)
+func.getInforTasks(IndClass, tasks)
+func.dim_uss = 9
+print(func(p1,p2))
+from tqdm import tqdm
+for i in tqdm(range(500 * 100 * 500)):
+    func(p1, p2)
 
-path = os.path.dirname(os.path.realpath(__file__))
-print(path)
