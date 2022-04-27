5dd37231ee6e99ec455867111f4161abbc34086e
kaito0311 <tanminh8b@gmail.com>
2022-04-27 14:35:48 +0700

    .

--- MFEA_lib/model/utils.py
+++ MFEA_lib/model/utils.py
@@ -74,12 +74,13 @@ class MultiTimeModel:
                 print(f'can not convert {self.list_attri_avg[i]} to np.array')
                 continue
 
-    def print_result(self, print_attr = []):
+    def print_result(self, print_attr = [], print_time = True, print_name= True):
         # print time
         seconds = self.total_time
         minutes = seconds // 60
         seconds = seconds - minutes * 60
-        print("total time: %02dm %.02fs" % (minutes, seconds))
+        if print_time: 
+            print("total time: %02dm %.02fs" % (minutes, seconds))
 
         # print avg
         if self.list_attri_avg is None:
@@ -91,7 +92,8 @@ class MultiTimeModel:
         for i in range(len(print_attr)):
             try:
                 result = self.__getattribute__(print_attr[i])[-1]
-                print(f"{print_attr[i]} avg: ")
+                if print_name: 
+                    print(f"{print_attr[i]} avg: ")
                 np.set_printoptions(
                     formatter={'float': lambda x: format(x, '.2E')})
                 print(result)
@@ -101,7 +103,8 @@ class MultiTimeModel:
                         print_attr[i]) for model in self.ls_model]
                     result = np.array(result)
                     result = np.sum(result, axis=0) / len(self.ls_model)
-                    print(f"{print_attr[i]} avg: ")
+                    if print_name: 
+                        print(f"{print_attr[i]} avg: ")
                     np.set_printoptions(
                         formatter={'float': lambda x: format(x, '.2E')})
                     print(result)
@@ -849,3 +852,42 @@ class TuningModel:
                     path += "/" + str(name_arg) + "/" + str(value)
                     index += 1
         return path
+
+class MultiBenchmark(): 
+    def __init__(self, ls_benchmark = [], name_benchmark = [], ls_IndClass = [], model : AbstractModel= None) :
+        self.ls_benchmark = ls_benchmark 
+        self.ls_name_benchmark = name_benchmark 
+        self.ls_IndClass = ls_IndClass 
+
+        self.model = model  
+        pass
+    
+    def compile(self, **kwargs): 
+        self.compile_kwargs = kwargs 
+    
+    def fit(self, **kwargs):
+        self.fit_kwargs = kwargs 
+    
+    def run(self,nb_run = 1, save_path = './RESULTS/result/'): 
+        self.ls_model = [] 
+        for idx, benchmark in enumerate(self.ls_benchmark): 
+            self.compile_kwargs['tasks'] = benchmark 
+            self.compile_kwargs['IndClass'] = self.ls_IndClass[idx] 
+
+            model = MultiTimeModel(model = self.model) 
+            model.compile(**self.compile_kwargs) 
+            model.fit(**self.fit_kwargs) 
+            model.run(nb_run = nb_run, save_path= save_path + self.ls_name_benchmark[idx] + ".mso") 
+            self.ls_model.append(model) 
+    
+    def print_result(self,print_attr = [],print_name = True, print_time = False, print_name_attr = False):
+        
+        for idx, model in enumerate(self.ls_model): 
+            if print_name : 
+                print(self.ls_name_benchmark[idx])
+            model.print_result(print_attr, print_time= print_time, print_name= print_name_attr) 
+
+
+        
+        
+
